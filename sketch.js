34b52c7c90d2912301b20d7d5e57fc8954e7e988
uniqemejiya <95008229+uniqemejiya@users.noreply.github.com>
2022-01-22 17:45:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+
+function preload(){
+  //pre-load images
+  trackImg = loadImage("path.png")
+  addImg = (path.png)
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+  createSprite = track;
+}
+
+function draw() {
+  background(0);
+
+}
